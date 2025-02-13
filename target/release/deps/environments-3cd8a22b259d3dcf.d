a776753bbf382e7deaa4ca0880e6b383d483c425
Sarra <95942823+sarrakou@users.noreply.github.com>
2025-02-13 23:50:25 +0100

    part 2 of dyna q and q learning trained models

--- target/release/deps/environments-3cd8a22b259d3dcf.d
+++ target/release/deps/environments-3cd8a22b259d3dcf.d
@@ -1,11 +1,13 @@
-C:\Users\sarra\RustroverProjects\ProjetDL\target\release\deps\libenvironments-3cd8a22b259d3dcf.rmeta: environments\src\lib.rs environments\src\line_world.rs environments\src\grid_world.rs environments\src\rps.rs environments\src\secret_env.rs
+C:\Users\sarra\RustroverProjects\ProjetDL\target\release\deps\libenvironments-3cd8a22b259d3dcf.rmeta: environments\src\lib.rs environments\src\line_world.rs environments\src\grid_world.rs environments\src\rps.rs environments\src\secret_env.rs environments\src\monty_hall_paradox1.rs environments\src\monty_hall_paradox2.rs
 
-C:\Users\sarra\RustroverProjects\ProjetDL\target\release\deps\libenvironments-3cd8a22b259d3dcf.rlib: environments\src\lib.rs environments\src\line_world.rs environments\src\grid_world.rs environments\src\rps.rs environments\src\secret_env.rs
+C:\Users\sarra\RustroverProjects\ProjetDL\target\release\deps\libenvironments-3cd8a22b259d3dcf.rlib: environments\src\lib.rs environments\src\line_world.rs environments\src\grid_world.rs environments\src\rps.rs environments\src\secret_env.rs environments\src\monty_hall_paradox1.rs environments\src\monty_hall_paradox2.rs
 
-C:\Users\sarra\RustroverProjects\ProjetDL\target\release\deps\environments-3cd8a22b259d3dcf.d: environments\src\lib.rs environments\src\line_world.rs environments\src\grid_world.rs environments\src\rps.rs environments\src\secret_env.rs
+C:\Users\sarra\RustroverProjects\ProjetDL\target\release\deps\environments-3cd8a22b259d3dcf.d: environments\src\lib.rs environments\src\line_world.rs environments\src\grid_world.rs environments\src\rps.rs environments\src\secret_env.rs environments\src\monty_hall_paradox1.rs environments\src\monty_hall_paradox2.rs
 
 environments\src\lib.rs:
 environments\src\line_world.rs:
 environments\src\grid_world.rs:
 environments\src\rps.rs:
 environments\src\secret_env.rs:
+environments\src\monty_hall_paradox1.rs:
+environments\src\monty_hall_paradox2.rs:
