ae8091ac31ddc5c98712bb9e64c030722115947c
ruth <45215676+RoutyS@users.noreply.github.com>
2025-02-13 16:13:15 +0100

    ajout Algo 3

--- algorithms/src/lib.rs
+++ algorithms/src/lib.rs
@@ -1,5 +1,7 @@
 pub mod q_learning;
 pub mod dyna_q;
+mod sarsa;
+mod SemiGradiantSarsa;
 
 pub trait RLAlgorithm: Send {
     fn train<T: environments::Environment + Clone>(&mut self, env: &mut T, max_episodes: usize) -> Vec<f32>;
